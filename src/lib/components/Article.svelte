226e31d3d0f34ee25a3a92d26b6d3af871abb542
Seania <seania.twix@gmail.com>
2022-07-29 15:41:01 +0900

    better ui article view page

---
+++ src/lib/components/Article.svelte
@@ -0,0 +1,253 @@
+<script lang="ts">
+  import Upload from 'svelte-material-icons/Upload.svelte';
+  import View from 'svelte-material-icons/Eye.svelte';
+  import Plus from 'svelte-material-icons/Plus.svelte';
+  import Favorite from 'svelte-material-icons/Star.svelte';
+  import Edit from 'svelte-material-icons/Pencil.svelte';
+  import Delete from 'svelte-material-icons/TrashCan.svelte';
+  import Report from 'svelte-material-icons/AlertBox.svelte';
+  import Up from 'svelte-material-icons/ArrowUp.svelte';
+  import Down from 'svelte-material-icons/ArrowDown.svelte';
+  import Back from 'svelte-material-icons/KeyboardBackspace.svelte';
+  import Admin from 'svelte-material-icons/Settings.svelte';
+  import Like from 'svelte-material-icons/ThumbUp.svelte';
+  import LikeEmpty from 'svelte-material-icons/ThumbUpOutline.svelte';
+  import Dislike from 'svelte-material-icons/ThumbDown.svelte';
+  import DislikeEmpty from 'svelte-material-icons/ThumbDownOutline.svelte';
+  import RemoveTag from 'svelte-material-icons/Close.svelte';
+
+  import type {IArticle} from '$lib/types/article';
+  import {session} from '$app/stores';
+  import {EUserRanks} from '$lib/types/user-ranks';
+  import TimeAgo from 'javascript-time-ago';
+  import {ko} from '$lib/time-ko';
+  import {dayjs} from 'dayjs';
+  import CircleAvatar from './CircleAvatar.svelte';
+  import Content from './Content.svelte';
+  import type {IUser} from '$lib/types/user';
+  import ky from 'ky-universal';
+  import Tag from './Tag.svelte';
+
+  export let article: IArticle;
+  export let users: Record<string, IUser>;
+  export let contents: string;
+
+  // noinspection TypeScriptUnresolvedFunction
+  let liked = article?.myTags?.includes('_like');
+  // noinspection TypeScriptUnresolvedFunction
+  let disliked = article?.myTags?.includes('_dislike');
+  $: likeCount = article?.tags?._like ?? 0;
+  $: dislikeCount = article?.tags?._dislike ?? 0;
+
+  TimeAgo.addLocale(ko as any);
+  const timeAgo = new TimeAgo('ko-KR');
+
+
+  function timeFullFormat(time: Date | number) {
+    return dayjs(new Date(time)).format('YYYY년 M월 D일 HH시 m분');
+  }
+
+  function toImageSource(uid: string): IImage {
+    // @ts-ignore
+    let avatar = users[uid]?.avatar;
+    if (!avatar) {
+      // @ts-ignore
+      avatar = article.author?.avatar;
+    }
+    if (!avatar) {
+      // fallback
+      avatar = 'https://s3.ru.hn/IMG_2775.GIF';
+    }
+    const type = avatar.split('.')[1];
+    return {src: avatar, type,};
+  }
+
+  function isMyTag(tagName: string): boolean {
+    // console.log(tagName, article.myTags)
+    // noinspection TypeScriptUnresolvedFunction
+    return article.myTags?.includes(tagName)
+  }
+
+  async function addTag(tags: string) {
+    const t = tags.includes(',')
+      ? tags.split(',').map(v => v.trim()).join(',') : tags.trim();
+    return await ky
+      .put(`/community/${article.board}/${article._key}/api/tag/add?name=${t}`)
+      .json();
+  }
+
+  async function removeTag(tags: string) {
+    const t = tags.includes(',')
+      ? tags.split(',').map(v => v.trim()).join(',') : tags.trim();
+    return await ky
+      .delete(`/community/${article.board}/${article._key}/api/tag/remove?name=${t}`)
+      .json();
+  }
+
+  async function vote(type: 'like' | 'dislike') {
+    // console.log(type);
+    if (type === 'like') {
+      if (liked) {
+        liked = false;
+        return removeTag('_like');
+      } else if (disliked) {
+        disliked = false;
+        await removeTag('_dislike');
+      }
+      // noinspection TypeScriptUnresolvedVariable
+      if ($session?.user?.uid !== article.author._key) {
+        liked = true;
+        return await addTag('_like');
+      }
+    } else { // vote dislike
+      if (disliked) {
+        disliked = false;
+        return removeTag('_dislike');
+      } else if (liked) {
+        liked = false;
+        await removeTag('_like');
+      }
+      // noinspection TypeScriptUnresolvedVariable
+      if ($session?.user?.uid !== article.author._key) {
+        disliked = true;
+        return await addTag('_dislike');
+      }
+    }
+  }
+
+
+  interface IImage {
+    src: string
+    type: string
+  }
+</script>
+<div class="w-11/12 sm:w-5/6 md:w-4/5 lg:w-3/5 mx-auto p-4 rounded-md shadow-md transition-transform divide-y divide-dotted">
+  <div class="space-y-2 mb-4">
+    <div class="flex justify-between">
+      <div class="flex space-x-2 flex-col md:flex-row lg:flex-row min-w-0">
+        <h2 class="text-2xl flex-shrink">{article.title}</h2>
+        <div class="inline-block flex space-x-2 pr-4">
+          {#if $session.user}
+            <div class="w-max py-2 md:py-0.5">
+              {#if $session.user.uid !== article.author._key}
+                    <span class="mt-0.5 cursor-pointer hover:text-red-600">
+                      <Report size="1rem"/>
+                    </span>
+              {/if}
+              {#if article.author._key === $session.user.uid}
+                <a href="/community/{article.board}/{article._key}/edit"
+                   class="inline-block mt-0.5 cursor-pointer hover:text-sky-400">
+                  <Edit size="1rem"/>
+                </a>
+              {/if}
+              {#if article.author._key === $session.user.uid || $session.user.rank >= EUserRanks.Manager}
+                    <span class="mt-0.5 cursor-pointer hover:text-red-400">
+                      <Delete size="1rem"/>
+                    </span>
+              {/if}
+              {#if $session.user.rank >= EUserRanks.Manager}
+                <a href="/community/{article.board}/{article._key}/manage" class="mt-0.5 cursor-pointer hover:text-red-400">
+                  <Admin size="1rem"/>
+                </a>
+              {/if}
+            </div>
+          {/if}
+        </div>
+      </div>
+      <div class="flex flex-col md:flex-col">
+        <span class="w-max"><View size="1rem"/> {article.views ?? 1}</span>
+
+        <button class="w-full text-right" data-tooltip-target="tooltip-time-specific" type="button">
+          <time class="text-zinc-500 dark:text-zinc-300 text-sm">
+            {timeAgo.format(new Date(article.createdAt))}
+          </time>
+        </button>
+
+        <div id="tooltip-time-specific" role="tooltip"
+             class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+          작성 시간: {timeFullFormat(article.createdAt)}
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+    </div>
+    <div class="flex space-x-3 items-center">
+      <div class="w-12 min-h-[3rem] inline-block">
+        <CircleAvatar fallback="{toImageSource(article.author._key)}"/>
+      </div>
+      <span class="inline-block leading-none hover:text-sky-400">{article.author?.id}</span>
+    </div>
+  </div>
+  {#if article.source}
+    <div class="rounded-sm overflow-hidden">
+      <p class="px-4 py-2 border-l-2 border-sky-400 select-none dark:bg-zinc-600">
+        출처 <a class="text-sky-300 hover:text-sky-400 transition-colors select-text"
+              href="{article.source}">{article.source}</a>
+      </p>
+    </div>
+  {/if}
+  <article class="pt-4 pb-2 min-h-[10rem]">
+    <Content contents="{contents}" nsfw="{!!article.tags['후방']}"/>
+  </article>
+  <div class="pt-3">
+    <ul class="space-x-2 flex flex-wrap">
+      {#if $session.user}
+        <li on:click={() => vote('like')}
+            class:cursor-not-allowed={$session.user.uid === article.author}
+            class:cursor-pointer={$session.user.uid !== article.author}
+            class="inline-block text-sky-400 hover:text-sky-600 mb-2" reserved>
+          <Tag>
+            {#if liked}
+              <Like size="1rem"/>
+            {:else}
+              <LikeEmpty size="1rem"/>
+            {/if}
+            {likeCount}
+          </Tag>
+        </li>
+        <li on:click={() => vote('dislike')}
+            class:cursor-not-allowed={$session.user.uid === article.author}
+            class:cursor-pointer={$session.user.uid !== article.author}
+            class="inline-block text-red-400 hover:text-red-600 mb-2" reserved>
+          <Tag>
+            {#if disliked}
+              <Dislike size="1rem"/>
+            {:else}
+              <DislikeEmpty size="1rem"/>
+            {/if}
+            {dislikeCount}
+          </Tag>
+        </li>
+      {:else}
+        <li class="inline-block text-sky-400 mb-2 cursor-default">
+          <Tag disabled="{true}">
+            <LikeEmpty size="1rem"/> {likeCount}
+          </Tag>
+        </li>
+        <li class="inline-block text-red-400 mb-2 cursor-default">
+          <Tag disabled="{true}">
+            <DislikeEmpty size="1rem"/> {dislikeCount}
+          </Tag>
+        </li>
+      {/if}
+      {#each Object.keys(article.tags) as tagName}
+        {#if !tagName.startsWith('_')}
+          <li class="inline-block mb-2 cursor-pointer">
+            <Tag count="{article.tags[tagName]}">{tagName}
+              {#if isMyTag(tagName)}<span class="text-gray-600 dark:text-gray-400 leading-none __icon-fix"><RemoveTag
+                size="1rem"/></span>{/if}
+            </Tag>
+          </li>
+        {/if}
+      {/each}
+
+      {#if $session.user}
+        <li class="inline-block mb-2 cursor-pointer">
+          <Tag>
+            <Plus size="1rem"/>
+            <span>새 태그 추가</span>
+          </Tag>
+        </li>
+      {/if}
+    </ul>
+  </div>
+</div>
--- src/routes/community/[id=integer]/[article=integer]/index.svelte
+++ src/routes/community/[id=integer]/[article=integer]/index.svelte
@@ -66,39 +66,24 @@
   }
 </script>
 <script lang="ts">
-  import TimeAgo from 'javascript-time-ago';
   import Upload from 'svelte-material-icons/Upload.svelte';
-  import View from 'svelte-material-icons/Eye.svelte';
-  import Plus from 'svelte-material-icons/Plus.svelte';
   import Favorite from 'svelte-material-icons/Star.svelte';
-  import Edit from 'svelte-material-icons/Pencil.svelte';
   import Delete from 'svelte-material-icons/TrashCan.svelte';
-  import Report from 'svelte-material-icons/AlertBox.svelte';
   import Up from 'svelte-material-icons/ArrowUp.svelte';
   import Down from 'svelte-material-icons/ArrowDown.svelte';
   import Back from 'svelte-material-icons/KeyboardBackspace.svelte';
-  import Admin from 'svelte-material-icons/Settings.svelte';
-  import Like from 'svelte-material-icons/ThumbUp.svelte';
-  import LikeEmpty from 'svelte-material-icons/ThumbUpOutline.svelte';
-  import Dislike from 'svelte-material-icons/ThumbDown.svelte';
-  import DislikeEmpty from 'svelte-material-icons/ThumbDownOutline.svelte';
   import RemoveTag from 'svelte-material-icons/Close.svelte';
   const Close = RemoveTag;
   import Goto from 'svelte-material-icons/ArrowRightBold.svelte';
-  import {ko} from '$lib/time-ko';
   import type {IArticle} from '$lib/types/article';
   import ky from 'ky-universal';
   import {onMount, onDestroy, tick} from 'svelte';
   import {Pusher} from '$lib/pusher/client';
   import {fade} from 'svelte/transition';
-  import CircleAvatar from '$lib/components/CircleAvatar.svelte';
   import {dayjs} from 'dayjs';
-  import {EUserRanks} from '$lib/types/user-ranks';
-  import Tag from '$lib/components/Tag.svelte';
   import Comment from '$lib/components/Comment.svelte';
   import type {Subscription} from 'rxjs';
   import {goto} from '$app/navigation';
-  import Content from '$lib/components/Content.svelte';
   import {writable} from 'svelte/store';
   import type {Unsubscriber} from 'svelte/store';
   import EditImage from '$lib/components/EditImage.svelte';
@@ -107,12 +92,11 @@
   import {page, session} from '$app/stores';
   import Cookies from 'js-cookie';
   import {currentReply} from '$lib/community/comment/client';
+  import Article from '$lib/components/Article.svelte';
 
   /**
    * 게시글 보기
    */
-  TimeAgo.addLocale(ko as any);
-  const timeAgo = new TimeAgo('ko-KR');
 
   interface TagType {
     [tagName: string]: number;
@@ -129,14 +113,7 @@
   export let mainImage: string | undefined;
   // noinspection TypeScriptUnresolvedVariable
   $: commentFolding = $session.commentFolding;
-  // noinspection TypeScriptUnresolvedFunction
-  let liked = article?.myTags?.includes('_like');
-  // noinspection TypeScriptUnresolvedFunction
-  let disliked = article?.myTags?.includes('_dislike');
-  $: likeCount = article?.tags?._like ?? 0;
-  $: dislikeCount = article?.tags?._dislike ?? 0;
 
-  let commentImageUrl = '';
   let commentTextInput: HTMLTextAreaElement;
   let commentContent = '';
 
@@ -238,61 +215,6 @@
     }
   }
 
-  function isMyTag(tagName: string): boolean {
-    // console.log(tagName, article.myTags)
-    // noinspection TypeScriptUnresolvedFunction
-    return article.myTags?.includes(tagName)
-  }
-
-  let prevVisualViewport = 0;
-
-  async function addTag(tags: string) {
-    const t = tags.includes(',')
-      ? tags.split(',').map(v => v.trim()).join(',') : tags.trim();
-    return await ky
-      .put(`/community/${article.board}/${article._key}/api/tag/add?name=${t}`)
-      .json();
-  }
-
-  async function removeTag(tags: string) {
-    const t = tags.includes(',')
-      ? tags.split(',').map(v => v.trim()).join(',') : tags.trim();
-    return await ky
-      .delete(`/community/${article.board}/${article._key}/api/tag/remove?name=${t}`)
-      .json();
-  }
-
-  async function vote(type: 'like' | 'dislike') {
-    // console.log(type);
-    if (type === 'like') {
-      if (liked) {
-        liked = false;
-        return removeTag('_like');
-      } else if (disliked) {
-        disliked = false;
-        await removeTag('_dislike');
-      }
-      // noinspection TypeScriptUnresolvedVariable
-      if ($session?.user?.uid !== article.author._key) {
-        liked = true;
-        return await addTag('_like');
-      }
-    } else { // vote dislike
-      if (disliked) {
-        disliked = false;
-        return removeTag('_dislike');
-      } else if (liked) {
-        liked = false;
-        await removeTag('_like');
-      }
-      // noinspection TypeScriptUnresolvedVariable
-      if ($session?.user?.uid !== article.author._key) {
-        disliked = true;
-        return await addTag('_dislike');
-      }
-    }
-  }
-
   async function userNameExistingCheck(author: string) {
     if (!users[author]) {
       try {
@@ -317,12 +239,6 @@
   function imageLoadCompletedInComment(element: HTMLImageElement) {
     URL.revokeObjectURL(element.src);
   }
-
-  // so hacky
-  //*
-  function focusOutTextArea(event: Event) {
-    console.log('body:', document.body.scrollHeight)
-  } //*/
 
   function fileSelected() {
     fileChangeListener.set(fileUploader.files[0]);
@@ -423,10 +339,6 @@
     window.addEventListener('unload', clearSubscribes);
 
     try {
-      prevVisualViewport = window.visualViewport.height;
-
-      // window.visualViewport.addEventListener('resize', fixIosKeyboardScrolling, true);
-
       ky.put(`/community/${article.board}/${article._key}/api/viewcount`).then();
 
       const whenCommentChanged = async ({body}) => {
@@ -531,7 +443,7 @@
       });
 
       const replyUnsub = currentReply.subscribe((currentReplyId) => {
-        // console.log(currentReplyId)
+        console.log(currentReplyId)
         if (!currentReplyId) {
           return;
         }
@@ -544,21 +456,6 @@
       console.error(e);
     }
   });
-
-  function toImageSource(uid: string): IImage {
-    // @ts-ignore
-    let avatar = users[uid]?.avatar;
-    if (!avatar) {
-      // @ts-ignore
-      avatar = article.author?.avatar;
-    }
-    if (!avatar) {
-      // fallback
-      avatar = 'https://s3.ru.hn/IMG_2775.GIF';
-    }
-    const type = avatar.split('.')[1];
-    return {src: avatar, type,};
-  }
 
   interface IImage {
     src: string
@@ -584,10 +481,6 @@
       // no window. it's ok.
     }
   });
-
-  function timeFullFormat(time: Date | number) {
-    return dayjs(new Date(time)).format('YYYY년 M월 D일 HH시 m분');
-  }
 
   interface IMessageVote {
     comment: string;
@@ -737,140 +630,12 @@
     </nav>
   </div>
 </div>
-<div in:fade class="flex flex-col justify-between" class:__fixed-view={!mobileInputMode}>
+<div in:fade class="flex flex-col justify-between fixed w-full" style="height: calc(100% - 62px);">
   {#if !mobileInputMode}
     <div bind:this={generalScrollView}
          class="mt-4 p-4 space-y-4 overflow-y-scroll flex-grow overflow-x-hidden">
-      <div class="w-11/12 sm:w-5/6 md:w-4/5 lg:w-3/5 mx-auto p-4 rounded-md shadow-md transition-transform divide-y divide-dotted">
-        <div class="space-y-2 mb-4">
-          <div class="flex justify-between">
-            <div class="flex space-x-2 flex-col md:flex-row lg:flex-row min-w-0">
-              <h2 class="text-2xl flex-shrink">{article.title}</h2>
-              <div class="inline-block flex space-x-2 pr-4">
-                {#if $session.user}
-                  <div class="w-max py-2 md:py-0.5">
-                    {#if $session.user.uid !== article.author._key}
-                    <span class="mt-0.5 cursor-pointer hover:text-red-600">
-                      <Report size="1rem"/>
-                    </span>
-                    {/if}
-                    {#if article.author._key === $session.user.uid}
-                      <a href="/community/{article.board}/{article._key}/edit"
-                         class="inline-block mt-0.5 cursor-pointer hover:text-sky-400">
-                        <Edit size="1rem"/>
-                      </a>
-                    {/if}
-                    {#if article.author._key === $session.user.uid || $session.user.rank >= EUserRanks.Manager}
-                    <span class="mt-0.5 cursor-pointer hover:text-red-400">
-                      <Delete size="1rem"/>
-                    </span>
-                    {/if}
-                    {#if $session.user.rank >= EUserRanks.Manager}
-                    <a href="/community/{article.board}/{article._key}/manage" class="mt-0.5 cursor-pointer hover:text-red-400">
-                      <Admin size="1rem"/>
-                    </a>
-                    {/if}
-                  </div>
-                {/if}
-              </div>
-            </div>
-            <div class="flex flex-col md:flex-col">
-              <span class="w-max"><View size="1rem"/> {article.views ?? 1}</span>
 
-              <button class="w-full text-right" data-tooltip-target="tooltip-time-specific" type="button">
-                <time class="text-zinc-500 dark:text-zinc-300 text-sm">
-                  {timeAgo.format(new Date(article.createdAt))}
-                </time>
-              </button>
-
-              <div id="tooltip-time-specific" role="tooltip"
-                   class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-                작성 시간: {timeFullFormat(article.createdAt)}
-                <div class="tooltip-arrow" data-popper-arrow></div>
-              </div>
-            </div>
-          </div>
-          <div class="flex space-x-3 items-center">
-            <div class="w-12 min-h-[3rem] inline-block">
-              <CircleAvatar fallback="{toImageSource(article.author._key)}"/>
-            </div>
-            <span class="inline-block leading-none hover:text-sky-400">{article.author?.id}</span>
-          </div>
-        </div>
-        {#if article.source}
-          <div class="rounded-sm overflow-hidden">
-            <p class="px-4 py-2 border-l-2 border-sky-400 select-none dark:bg-zinc-600">
-              출처 <a class="text-sky-300 hover:text-sky-400 transition-colors select-text"
-                    href="{article.source}">{article.source}</a>
-            </p>
-          </div>
-        {/if}
-        <article class="pt-4 pb-2 min-h-[10rem]">
-          <Content contents="{contents}" nsfw="{!!article.tags['후방']}"/>
-        </article>
-        <div class="pt-3">
-          <ul class="space-x-2 flex flex-wrap">
-            {#if $session.user}
-              <li on:click={() => vote('like')}
-                  class:cursor-not-allowed={$session.user.uid === article.author}
-                  class:cursor-pointer={$session.user.uid !== article.author}
-                  class="inline-block text-sky-400 hover:text-sky-600 mb-2" reserved>
-                <Tag>
-                  {#if liked}
-                    <Like size="1rem"/>
-                  {:else}
-                    <LikeEmpty size="1rem"/>
-                  {/if}
-                  {likeCount}
-                </Tag>
-              </li>
-              <li on:click={() => vote('dislike')}
-                  class:cursor-not-allowed={$session.user.uid === article.author}
-                  class:cursor-pointer={$session.user.uid !== article.author}
-                  class="inline-block text-red-400 hover:text-red-600 mb-2" reserved>
-                <Tag>
-                  {#if disliked}
-                    <Dislike size="1rem"/>
-                  {:else}
-                    <DislikeEmpty size="1rem"/>
-                  {/if}
-                  {dislikeCount}
-                </Tag>
-              </li>
-            {:else}
-              <li class="inline-block text-sky-400 mb-2 cursor-default">
-                <Tag disabled="{true}">
-                  <LikeEmpty size="1rem"/> {likeCount}
-                </Tag>
-              </li>
-              <li class="inline-block text-red-400 mb-2 cursor-default">
-                <Tag disabled="{true}">
-                  <DislikeEmpty size="1rem"/> {dislikeCount}
-                </Tag>
-              </li>
-            {/if}
-            {#each Object.keys(article.tags) as tagName}
-              {#if !tagName.startsWith('_')}
-                <li class="inline-block mb-2 cursor-pointer">
-                  <Tag count="{article.tags[tagName]}">{tagName}
-                    {#if isMyTag(tagName)}<span class="text-gray-600 dark:text-gray-400 leading-none __icon-fix"><RemoveTag
-                      size="1rem"/></span>{/if}
-                  </Tag>
-                </li>
-              {/if}
-            {/each}
-
-            {#if $session.user}
-              <li class="inline-block mb-2 cursor-pointer">
-                <Tag>
-                  <Plus size="1rem"/>
-                  <span>새 태그 추가</span>
-                </Tag>
-              </li>
-            {/if}
-          </ul>
-        </div>
-      </div>
+      <Article {article} {contents} {users} />
 
       {#if Object.keys(article.tags).find(tag => tag.startsWith('연재:'))}
         <div class="w-11/12 sm:w-5/6 md:w-4/5 lg:w-3/5 mx-auto">
@@ -896,6 +661,7 @@
         {#if selectedComment}
           <Comment comment="{selectedComment}"
                    article="{article._key}"
+                   allComments="{comments}"
                    myVote="{selectedComment.myVote}"
                    isReplyMode="{true}"
                    {users} />
@@ -908,6 +674,7 @@
           </div>
 
         {:else}
+
           <ul class="space-y-3">
 
             {#each noRelativeComments as comment}
@@ -932,6 +699,7 @@
               끝
             {/if}
           </p>
+
         {/if}
       </div>
     </div>
@@ -1026,22 +794,31 @@
           </li>
         </ul>
       </div>
+
       {#if $session.user}
+
         <div class="overflow-hidden rounded-t-md shadow-md bg-gray-50/50 flex flex-col relative __comment-input">
           <div class:__ios-bottom-fix={commentFolding} class="px-2 flex flex-row {!selectedComment ? 'hover:bg-gray-200 dark:hover:bg-gray-300/80' : ''} items-center transition-all leading-zero">
+
             {#if isEmpty(commentImageUploadSrc)}
+
               <button class="text-zinc-700 hover:text-zinc-900 p-1 cursor-pointer">
                 <Favorite size="1.25rem"/>
               </button>
               <button on:click={() => fileUploader.click()} class="text-zinc-700 hover:text-zinc-900 p-1 cursor-pointer">
                 <Upload size="1.25rem"/>
               </button>
+
             {:else}
+
               <button on:click={cancelImageUpload} class="text-zinc-700 hover:text-red-600 p-1 cursor-pointer">
                 <span class="text-lg"><Delete size="1.25rem"/> 파일을 지우려면 여기 클릭하세요</span>
               </button>
+
             {/if}
+
             {#if selectedComment}
+
               <div class="h-8 flex-grow flex flex-row justify-end text-lg text-zinc-600 dark:text-gray-700 cursor-default select-none w-0">
                 <p class="inline-block flex flex-row min-w-0 pl-4">
                   <span class="min-w-fit">{users[selectedComment.author].id}님의 "</span>
@@ -1056,21 +833,28 @@
                   </span>
                 </p>
               </div>
+
             {:else}
+
               <button on:click={toggleCommentFold} class="h-8 flex-grow cursor-pointer items-center">
                 <!-- Fold Toggle -->
               </button>
+
             {/if}
           </div>
           <div class="w-full flex flex-row grow shrink-0 {commentFolding ? 'h-0' : 'h-24'} transition-all">
             <div class="flex flex-grow">
+
               {#if !isEmpty(commentImageUploadSrc)}
+
                 <div on:click={openImageEditor} class="flex-shrink-0 w-24 border-4 border-zinc-100 dark:border-gray-300/50 hover:border-sky-400 dark:hover:border-sky-500 cursor-pointer select-none">
                   <img class="w-full h-full object-cover bg-white dark:bg-gray-600"
                        on:load={imageLoadCompletedInComment}
                        src="{commentImageUploadSrc}" alt="upload preview" />
                 </div>
+
               {/if}
+
               <div class="bg-gray-100 dark:bg-gray-300 p-3 flex-grow shadow-md dark:text-gray-800 h-full relative">
                 <textarea id="__textarea-general" class="w-full h-full bg-transparent focus:outline-none overflow-y-scroll overscroll-contain resize-none touch-none"
                           on:keydown={detectSendOrEsc}
@@ -1082,18 +866,28 @@
                 <div id="__textarea-mobile"
                      on:click={enableMobileInput} on:dblclick|preventDefault
                      class="w-full h-full bg-transparent focus:outline-none overflow-y-scroll overscroll-contain resize-none touch-none">
+
                   {#if isEmpty(commentContent)}
+
                     <span class="text-[#9DA3AE]">댓글을 입력하세요...</span>
+
                   {:else}
+
                     {commentContent}
+
                   {/if}
+
                 </div>
               </div>
             </div>
             <button on:click={addComment} class="px-4 bg-sky-200 dark:bg-sky-800">
+
               {#if selectedComment}
+
                 답글
+
               {/if}
+
               작성
             </button>
           </div>
@@ -1104,16 +898,6 @@
 </div>
 
 <style lang="scss">
-  //noinspection CssInvalidPropertyValue
-  .__fixed-view {
-    height: calc(100vh - 62px);
-    // for mobile
-    @supports (-webkit-touch-callout: none) {
-      height: calc(var(--vh, 1vh) * 100 - 62px );
-      // padding-bottom: env(safe-area-inset-bottom);
-    }
-  }
-
   .__ios-bottom-fix {
     padding-bottom: 0;
     //noinspection CssOverwrittenProperties
