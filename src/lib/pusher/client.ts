73d0ae9199f87e413cff4f296c8033e17435585c
Seania <seania.twix@gmail.com>
2022-07-31 06:04:46 +0900

    add omitted file

--- src/lib/pusher/client.ts
+++ src/lib/pusher/client.ts
@@ -1,6 +1,5 @@
-import {Subject} from 'rxjs';
+import {Subject, Subscription} from 'rxjs';
 import type {Observable} from 'rxjs';
-import {isEmpty} from 'lodash-es';
 import type {PushAbout} from './shared';
 
 try {
@@ -16,53 +15,91 @@ try {
 
 export class Pusher {
   private readonly target: string;
-  private wsList: WebSocket[] = [];
-  private subjects: Subject<any>[] = [];
+  private sockets: Record<string, WebSocket> = {};
+  private subjects: Record<string, Subject<{ body: any, socket: WebSocket }>> = {};
+  private subscriptions: Record<string, Subscription> = {};
   private timers: number[] = [];
+
+  private reconnecting = false;
 
   constructor(context: string) {
     this.target = context;
+    if (window) {
+      window.addEventListener('unload', this.close);
+      navigator.connection.addEventListener('change', this.networkChanged, true);
+      window.addEventListener('online', this.online, true);
+      window.addEventListener('offline', this.offline, true);
+    }
+  }
+
+  private networkChanged() {
+    console.log('network changed');
+    // todo reconnect
+  }
+
+  destory() {
+    this.close();
+    navigator.connection.removeEventListener('change', this.networkChanged, true);
+    window.removeEventListener('online', this.online, true);
+    window.removeEventListener('offline', this.offline, true);
+  }
+
+  private online() {
+    this.reconnecting = true;
+  }
+
+  private offline() {
+    this.reconnecting = false;
   }
 
   close() {
-    for (const subject of this.subjects) {
-      try {
-        subject.complete();
-        subject.unsubscribe();
-      } catch (e) {
-        console.log('pusher:', e);
-      }
+    for (const url in this.sockets) {
+      console.log('close:', url);
+      this.clearConnection(url);
+    }
+  }
+
+  clearConnection(url: string | URL) {
+    const subject = this.subjects[url.toString()];
+    if (subject) {
+      subject.complete();
+      subject.unsubscribe();
     }
 
-    for (const ws of this.wsList) {
+    delete this.subjects[url.toString()];
+
+    const ws = this.sockets[url.toString()];
+    if (ws) {
       if (ws.readyState === WebSocket.OPEN) {
         ws.send('command:close');
       }
       ws.close(1000);
     }
-  }
 
-  private connect() {
+    delete this.sockets[url.toString()];
 
-  }
-
-  observable<T>(about: PushAbout): Observable<{ body: T, socket: WebSocket }> {
-    const dev = window.location.host.startsWith('localhost') || window.location.host.startsWith('192');
-    const url = dev ? 'ws://localhost:50000' : 'wss://push.ru.hn';
-    const ws = new WebSocket(`${url}/subscribe/${this.target}/${about}`);
-
-    if (isEmpty(this.wsList)) {
-      window.onbeforeunload = () => {
-        this.close();
-      };
+    const subscription = this.subscriptions[url.toString()];
+    if (subscription) {
+      subscription.unsubscribe();
     }
 
-    this.wsList.push(ws);
-    const subject = new Subject<{ body: T, socket: WebSocket }>();
-    this.subjects.push(subject);
+    delete this.subscriptions[url.toString()];
+  }
+
+  private connect(url: string | URL) {
+    if (this.sockets[url.toString()]) {
+      this.clearConnection(url);
+    }
+
+    this.sockets[url.toString()] = new WebSocket(url);
+
+    const ws = this.sockets[url.toString()];
+    const subject = new Subject<{ body: any, socket: WebSocket }>();
+
+    this.subjects[url.toString()] = subject;
+
     ws.onmessage = (event) => {
       try {
-
         let body = JSON.parse(event.data);
 
         // hack for escaping backslash
@@ -82,7 +119,7 @@ export class Pusher {
       }
     };
 
-    ws.onclose = (close) => {
+    ws.onclose = () => {
       this.timers.push(
         setInterval(() => {
           //
@@ -90,6 +127,30 @@ export class Pusher {
       )
     }
 
+    return subject;
+  }
+
+  private static GetURL() {
+    const dev = window.location.host.startsWith('localhost') || window.location.host.startsWith('192');
+    return dev ? 'ws://localhost:50000' : 'wss://push.ru.hn';
+  }
+
+  observable<T>(about: PushAbout): Observable<{ body: T, socket: WebSocket }> {
+    const url = Pusher.GetURL();
+    const subject = this.connect(`${url}/subscribe/${this.target}/${about}`);
+
+    // todo: listen event network changed, set online offline
+
     return subject.asObservable();
+  }
+
+  subscribe<T>(about: PushAbout, when: (data: { body: T, socket: WebSocket }) => void): Subscription {
+    const obs = this.observable<T>(about);
+    const subscription = obs.subscribe(when);
+
+    const url = Pusher.GetURL();
+    this.subscriptions[`${url}/subscribe/${this.target}/${about}`] = subscription;
+
+    return subscription;
   }
 }
